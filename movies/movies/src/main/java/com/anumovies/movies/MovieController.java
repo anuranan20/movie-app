11d9e6dc541591e1aae108107700bdd1449f5b1f
Anuranan <anuranangoswami@gmail.com>
2023-07-03 22:52:53 +0530

    commit first

---
+++ movies/movies/src/main/java/com/anumovies/movies/MovieController.java
@@ -0,0 +1,34 @@
+package com.anumovies.movies;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import org.bson.types.ObjectId;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+@CrossOrigin(origins = "*")
+public class MovieController {
+
+    @Autowired
+
+    private MovieService movieService;
+    @GetMapping
+
+    public ResponseEntity<List<Movie>> getallMovies(){
+
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    //finding movies by ID
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId){
+
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId), HttpStatus.OK);
+    }
+}
